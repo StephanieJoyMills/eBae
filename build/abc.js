bfb89cc4b80858760fb81be6bfef924af12e0421
Oscar Shi <oscar.shi@uber.com>
2020-03-07 22:39:50 -0800

    stuff

--- build/abc.js
+++ build/abc.js
@@ -1,4 +1,3 @@
 $(document).ready(function(){
-  alert("HI");
   chrome.runtime.sendMessage({greeting: "hello"});
 })
--- content/src/scripts/components/app/App.jsx
+++ content/src/scripts/components/app/App.jsx
@@ -12,11 +12,17 @@ import InputLabel from "@material-ui/core/InputLabel";
 import OutlinedInput from "@material-ui/core/OutlinedInput";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import Button from "@material-ui/core/Button";
+import axios from "axios";
 const mapStateToProps = (state) => {
   return {
     count: state.count
   };
 };
+
+var key = "";
+var fulfillmentId = -1;
+var paymentId = -1;
+var returnPolicyId = -1;
 
 const useStyles = makeStyles((theme) => ({
   // root: {
@@ -34,6 +40,209 @@ const useStyles = makeStyles((theme) => ({
     width: 38
   }
 }));
+
+function getKey(){
+  return axios.request({
+    method: "get",
+    url: "https://23.100.26.70/get"
+  });
+}
+
+function makeEbayRequest(key, data, url){
+  return axios.request({
+    method: "post",
+    url: url,
+    data: data,
+    headers: {
+      "Authorization": `Bearer ${key}`,
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+      "Content-Language": "en-US"
+    }
+  });
+}
+
+function createReturnPolicy(key, accepted){
+  return makeEbayRequest(key, {
+    name: "eBae default",
+    marketplaceId: "EBAY_US",
+    refundMethod: "MONEY_BACK",
+    returnsAccepted: accepted,
+    returnShippingCostPayer: "BUYER",
+    returnPeriod: {
+      value: 30,
+      unit: "DAY"
+    }
+  },
+  "https://api.ebay.com/sell/account/v1/return_policy");
+}
+
+function createPaymentPolicy(key){
+  return makeEbayRequest(key, {
+    name: "eBaeDefault",
+    marketplaceId: "EBAY_US",
+		categoryTypes: [
+				{
+					name: "ALL_EXCLUDING_MOTORS_VEHICLES"
+				}
+			],
+		paymentMethods: [
+			{
+				brands: ["VISA", "MASTERCARD"],
+				paymentMethodType: "CREDIT_CARD"
+			}
+		]
+  },
+  "https://api.ebay.com/sell/account/v1/payment_policy");
+}
+
+function createFulfillmentPolicy(key, shippingCarrierCode, shippingServiceCode){
+  return makeEbayRequest(key, {
+    name: "eBaeDefault",
+    marketplaceId: "EBAY_US",
+		categoryTypes: [
+				{
+					name: "ALL_EXCLUDING_MOTORS_VEHICLES"
+				}
+			],
+		globalShipping: "false",
+		handlingTime: {
+      unit: "DAY",
+      value: "1"
+		},
+    shippingOption: [
+      {
+        costType: "FLAT_RATE",
+        optionType: "DOMESTIC",
+        shippingService: [
+          {
+            buyerResponsibleForShipping: "false",
+            freeShipping: "true",
+            shippingCarrierCode: shippingCarrierCode,
+            shippingServiceCode: shippingServiceCode,
+            shippingCost: {
+              currency: "USD",
+              value: "0.0"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  "https://api.ebay.com/sell/account/v1/fulfillment_policy");
+}
+
+function createItem(key, sku, itemName, aspects, description, imgurl){
+  return makeEbayRequest(key, {
+    product:  {
+      title: itemName,
+      aspects: aspects,
+      description: description,
+      imageUrls: [
+        imgurl
+      ]
+    },
+    condition: "NEW"
+  },
+  `https://api.ebay.com/sell/inventory/v1/inventory_item/${SKU}`);
+}
+
+function createOffer(key, sku, price, locationId, fulfillmentId, paymentId, returnPolicyId, description){
+  return makeEbayRequest(key, {
+    sku: sku,
+    marketplaceId: "EBAY_US",
+    format: "FIXED_PRICE",
+    availableQuantity: 9999,
+    merchantLocationKey: locationId,
+    categoryId: 30120, // could replace with call to suggestions in the futureu
+    listingDescription: description,
+    listingPolicies: {
+      fulfillmentPolicyId: fulfillmentId,
+      paymentPolicyId: paymentId,
+      returnPolicyId: returnPolicyId
+    },
+    pricingSummary: {
+      price: {
+        currency: "USD",
+        value: price
+      }
+    }
+  },
+  `https://api.ebay.com/sell/inventory/v1/offer`);
+}
+
+function publishOffer(key, offerId){
+  makeEbayRequest(key, {
+  },
+    `https://api.ebay.com/sell/inventory/v1/offer/${offerId}/publish`);
+}
+
+function registerLocation(key, name, addressLine1, addressLine2, city, state, postalCode, country){
+  return makeEbayRequest(key, {
+    location: {
+      address: {
+        addressLine1: addressLine1,
+        addressLine2: addressLine2,
+        city: city,
+        state: state,
+        postalCode: postalCode,
+        country: country
+      }
+    },
+    name: name,
+    merchantLocationStatus: "ENABLED",
+    locationTypes: [
+      "Home"
+    ]
+  },
+  "https://api.ebay.com/sell/inventory/v1/location/1")
+}
+
+function checkFailed (then) {
+  return function (responses) {
+    const someFailed = responses.some(response => response.error)
+
+    if (someFailed) {
+      throw responses
+    }
+
+    return then(responses)
+  }
+}
+
+function registerIds(){
+  const promises = [
+    createReturnPolicy(key, true),
+    createPaymentPolicy(key),
+    createFulfillmentPolicy(key, "USPS", "USPSPriorityFlatRateBox")
+  ]
+  const promisesResolved = promises.map(promise => promise.catch(error => ({ error })));
+  axios.all(promisesResolved)
+  .then(checkFailed(function(returnPolicy, paymentPolicy, fulfillmentPolicy){
+    returnPolicyId = returnPolicy.data.returnPolicyId;
+    paymentId = paymentPolicy.data.paymentPolicyId;
+    fulfillmentId = fulfillmentPolicy.data.fulfillmentPolicyId;
+  })).catch(errors => {
+		returnPolicyId = errors[0].error.response.data.errors[0].parameters[0].value;
+		paymentId = errors[1].error.response.data.errors[0].parameters[2].value;
+		fulfillmentId = errors[2].error.response.data.errors[0].parameters[0].value;
+  });
+	console.log(returnPolicyId, paymentId, fulfillmentId);
+}
+
+function enroll(){
+  getKey().then(function(resp)
+    {
+      key = resp.data;
+      registerIds();
+    });
+}
+
+function handleClick(){
+  let sku = Math.floor(Math.random()*1000000);
+  createItem(key, sku, itemName, aspects, description, imgurl)
+  .then(() => createOffer(key, sku, price, locationId, fulfillmentId, paymentId, returnPolicyId, description).then((resp) => publishOffer(key, resp.data.offerId)));
+}
 
 function App(props) {
   const classes = useStyles();
@@ -58,6 +267,7 @@ function App(props) {
     //   });
     // });
   }, []);
+
 
   return (
     <div
@@ -203,8 +413,17 @@ function App(props) {
                   variant="contained"
                   color="primary"
                   style={{ padding: "5px 30px" }}
+                  onClick={(e) => handleClick()}
                 >
                   Add to eBay
+                </Button>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  style={{ padding: "5px 30px" }}
+                  onClick={(e) => enroll()}
+                >
+                  Add to eBay2
                 </Button>
               </div>
             </CardContent>
