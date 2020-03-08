527f01f7a9b1c31715d5b2668a56711ec20d96d9
Oscar Shi <oscar.shi@uber.com>
2020-03-08 05:18:43 -0700

    fix merge conflcits

--- ext/background/src/background.js
+++ ext/background/src/background.js
@@ -1,9 +1,4 @@
 chrome.runtime.onMessage.addListener(function(message, callback) {
-<<<<<<< HEAD
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tab) {
-    chrome.tabs.update({ url: "localhost:3000" });
-  });
-=======
   if(message.greeting == "redirect"){
     chrome.tabs.query({active:true, currentWindow:true}, function(tab) {
       chrome.tabs.update({url: "http://23.100.26.70"});
@@ -13,7 +8,6 @@ chrome.runtime.onMessage.addListener(function(message, callback) {
       chrome.tabs.remove(tab[0].id);
     });
   }
->>>>>>> eb50f319dc74b8f21ae14d7b6473fff31b44eab9
 });
 
 let addToStore = function(info, tab) {
@@ -48,13 +42,9 @@ function getword(info, tab) {
       chrome.tabs.executeScript(tab.id, { file: "content.js" });
     }
   );
-<<<<<<< HEAD
-  // chrome.tabs.create({url:"https://auth.ebay.com/oauth2/authorize?client_id=OscarShi-eBae-PRD-069eabc89-3a165e8f&response_type=code&redirect_uri=Oscar_Shi-OscarShi-eBae-P-pqjdex&scope=https://api.ebay.com/oauth/api_scope https://api.ebay.com/oauth/api_scope/sell.marketing.readonly https://api.ebay.com/oauth/api_scope/sell.marketing https://api.ebay.com/oauth/api_scope/sell.inventory.readonly https://api.ebay.com/oauth/api_scope/sell.inventory https://api.ebay.com/oauth/api_scope/sell.account.readonly https://api.ebay.com/oauth/api_scope/sell.account https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly https://api.ebay.com/oauth/api_scope/sell.fulfillment https://api.ebay.com/oauth/api_scope/sell.analytics.readonly https://api.ebay.com/oauth/api_scope/sell.finances https://api.ebay.com/oauth/api_scope/sell.payment.dispute https://api.ebay.com/oauth/api_scope/commerce.identity.readonly"});
-=======
   $.get("https://23.100.26.70/get", function(data){
     console.log(data);
     if(data.length < 2)
 	chrome.tabs.create({url:"https://auth.ebay.com/oauth2/authorize?client_id=OscarShi-eBae-PRD-069eabc89-3a165e8f&response_type=code&redirect_uri=Oscar_Shi-OscarShi-eBae-P-pqjdex&scope=https://api.ebay.com/oauth/api_scope https://api.ebay.com/oauth/api_scope/sell.marketing.readonly https://api.ebay.com/oauth/api_scope/sell.marketing https://api.ebay.com/oauth/api_scope/sell.inventory.readonly https://api.ebay.com/oauth/api_scope/sell.inventory https://api.ebay.com/oauth/api_scope/sell.account.readonly https://api.ebay.com/oauth/api_scope/sell.account https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly https://api.ebay.com/oauth/api_scope/sell.fulfillment https://api.ebay.com/oauth/api_scope/sell.analytics.readonly https://api.ebay.com/oauth/api_scope/sell.finances https://api.ebay.com/oauth/api_scope/sell.payment.dispute https://api.ebay.com/oauth/api_scope/commerce.identity.readonly"});
   });
->>>>>>> eb50f319dc74b8f21ae14d7b6473fff31b44eab9
 }
